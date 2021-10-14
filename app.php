a990b82852b424b3ad1d27a7d49494d6c55dcb31
rzarandona <rein.zarandona@gmail.com>
2021-10-14 20:39:12 +0800

    dev

--- app.php
+++ app.php
@@ -17,13 +17,14 @@
   <link rel="stylesheet" href="https://bootswatch.com/5/united/bootstrap.min.css">
 </head>
 <body>
-  <div class="container">
-    <nav class="navbar navbar-light bg-light">
-      <div class="container-fluid">
+
+    <nav class="navbar navbar-dark bg-primary">
+      <div class="container">
         <a class="navbar-brand" href="http://157.245.51.194/api/hectors_post/be_brave/app.php">Book Submissions API Dashboard</a>
       </div>
     </nav>
-    <?php print_r($book_submissions)?>
-  </div>
+    <div class="container">
+      <?php print_r($book_submissions)?>
+    </div>
 </body>
 </html>
