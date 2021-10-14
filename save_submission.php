ad0c416dab08ed2b82ac516ea78634553ee0b500
rzarandona <rein.zarandona@gmail.com>
2021-10-14 17:43:15 +0800

    dev

--- save_submission.php
+++ save_submission.php
@@ -14,17 +14,20 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-
 $email = $_POST['email'];
 $outer_pdf_link = $_POST['outer_pdf_url'];
 $inner_pdf_link = $_POST['inner_pdf_url'];
 $preview_image_link = $_POST['preview_url'];
 
-$data = [
-  $email,
-  $outer_pdf_link,
-  $inner_pdf_link,
-  $preview_image_link
-];
+$sql = "
+INSERT INTO 
+  book_submissions 
+    (email, preview_image_link, outer_pdf_link, inner_pdf_link, date_created, date_paid, is_paid)
+  VALUES
+    ('{$email}', '{$preview_image_link}', '{$outer_pdf_link}', '{$inner_pdf_link}', '{$date_now}', NULL, 'false');
+";
 
-echo json_encode($data);
+
+
+
+echo json_encode($sql);
