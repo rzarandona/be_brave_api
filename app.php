c39a4f1f4c63f14ec8ac64ca990be03022cae896
rzarandona <rein.zarandona@gmail.com>
2021-10-14 20:37:28 +0800

    dev

--- app.php
+++ app.php
@@ -14,11 +14,11 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Book Submissions API Dashboard</title>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+  <link rel="stylesheet" href="https://bootswatch.com/5/united/bootstrap.min.css">
 </head>
 <body>
   <div class="container">
-    <h1>Book Submissions API Dashboard</h1>
+    <h3>Book Submissions API Dashboard</h3>
     <?php print_r($book_submissions)?>
   </div>
 </body>
