a372e65384fd5310e916bd503478f48c8f7acb89
rzarandona <rein.zarandona@gmail.com>
2021-10-14 17:54:53 +0800

    dev

--- save_submission.php
+++ save_submission.php
@@ -5,7 +5,7 @@ header('Access-Control-Allow-Origin: *');
 $servername = "localhost";
 $username = "root";
 $password = "#2021Wearetraction";
-$database = "book_submissions";
+$database = "hectors_post";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);
