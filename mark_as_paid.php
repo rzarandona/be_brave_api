54b78738e57fb7e8088edbbd92b4c081ff26e48d
rzarandona <rein.zarandona@gmail.com>
2021-10-15 15:37:05 +0800

    dev

--- mark_as_paid.php
+++ mark_as_paid.php
@@ -3,12 +3,14 @@ require("dbconn.php");
 header('Access-Control-Allow-Origin: *');
 
 $session_id = $_POST['session_id'];
+$order_id = $_POST['order_id'];
 date_default_timezone_set('Europe/London');
 $date_now = date("m-d-Y h:i A");
 
+
 $sql = "
   UPDATE book_submissions
-  SET is_paid = 'true', date_paid = '{$date_now}'
+  SET is_paid = 'true', date_paid = '{$date_now}', order_id = '{$order_id}'
   WHERE id = {$session_id}
 ";
 
