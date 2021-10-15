6e31942ce3d712de9790ff60ad1b83c2bcc3a443
rzarandona <rein.zarandona@gmail.com>
2021-10-15 15:10:08 +0800

    dev

--- mark_as_paid.php
+++ mark_as_paid.php
@@ -3,4 +3,14 @@ require("dbconn.php");
 header('Access-Control-Allow-Origin: *');
 
 $session_id = $_POST['session_id'];
-echo $session_id;
+$date_now = date("m-d-Y h:i A");
+
+$sql = "
+  UPDATE book_submissions
+  SET is_paid = 'true', date_paid = '{$date_now}'
+  WHERE id = {$session_id}
+";
+
+if ($conn->query($sql) == TRUE) {
+  echo "UPDATED";
+}
