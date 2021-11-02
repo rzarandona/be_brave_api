0dab8da3bb580f0b894e163537025d6c9251cdae
rzarandona <rein.zarandona@gmail.com>
2021-11-02 13:35:48 +0800

    dev

--- create.php
+++ create.php
@@ -15,9 +15,6 @@ $s_pronoun =  $_GET['s_pronoun'] ?? "he";
 $o_pronoun =  $_GET['o_pronoun'] ?? "him";
 $p_pronoun =  $_GET['p_pronoun'] ?? "his";
 
-echo $character;
-exit;
-
 $uuid = Uuid::uuid4();
 $uuid_file_string = $uuid->toString();
 
