344f23e65fb9ff513d7275493b1d733097343a37
rzarandona <rein.zarandona@gmail.com>
2021-11-09 00:52:23 +0800

    dev

--- create.php
+++ create.php
@@ -71,7 +71,7 @@ curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($pdf_data));
 $response = curl_exec($ch);
 curl_close($ch);
-$pdf_converted_url = json_decode($response);
+$pdf_converted_url = json_decode($response)->document;
 
 //START OUTER CONVERSIONS
 $outer_pdf_data = [
@@ -92,7 +92,7 @@ curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($outer_pdf_data));
 $response = curl_exec($ch);
 curl_close($ch);
-$outer_pdf_converted_url = json_decode($response);
+$outer_pdf_converted_url = json_decode($response)->document;
 
 
 // START IMAGE CONVERSIONS
@@ -112,7 +112,7 @@ curl_setopt($ch2, CURLOPT_POSTFIELDS, json_encode($img_data));
 $response2 = curl_exec($ch2);
 curl_close($ch2);
 
-$img_converted_url = json_decode($response2);
+$img_converted_url = json_decode($response2)->document;
 
 
 
