a64e345d585e83bdb6121cd5c9a2c75d61cf89b1
rzarandona <rein.zarandona@gmail.com>
2021-11-11 08:50:08 +0800

    dev

--- save_submission.php
+++ save_submission.php
@@ -13,15 +13,18 @@ $outer_pdf_link = $_POST['outer_pdf_url'];
 $inner_pdf_link = $_POST['inner_pdf_url'];
 $preview_image_link = $_POST['preview_url'];
 
+$cover_type = $_POST['cover_type'];
+$sku = $_POST['sku'];
+
 date_default_timezone_set('Europe/London');
 $date_now = date("m-d-Y h:i A");
 
 $sql = "
 INSERT INTO 
   book_submissions 
-    (email, preview_image_link, outer_pdf_link, inner_pdf_link, date_created, date_paid, is_paid)
+    (email, preview_image_link, outer_pdf_link, inner_pdf_link, date_created, date_paid, is_paid, cover_type, sku)
   VALUES
-    ('{$email}', '{$preview_image_link}', '{$outer_pdf_link}', '{$inner_pdf_link}', '{$date_now}', NULL, 'false');
+    ('{$email}', '{$preview_image_link}', '{$outer_pdf_link}', '{$inner_pdf_link}', '{$date_now}', NULL, 'false', '${cover_type}', '${sku}');
 ";
 
 if ($conn->query($sql) == TRUE) {
